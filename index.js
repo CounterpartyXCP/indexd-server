9a7c614e1032706fc66bd23d3da61e08e5a536e7
John Villar <johnvillarzavatti@gmail.com>
2018-10-13 22:46:59 -0400

    Added regtest support

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 let dotenv = require('dotenv')
 require('dotenv').load({path: process.env.CONFIG_FILE ? process.env.CONFIG_FILE : '.env'})
 
-let TESTNET = (process.env.TESTNET === '1' || process.env.TESTNET.toLowerCase() === 'true')
+let TESTNET = (process.env.TESTNET === '1' || (process.env.TESTNET && process.env.TESTNET.toLowerCase() === 'true'))
+let REGTEST = (process.env.REGTEST === '1' || (process.env.REGTEST && process.env.REGTEST.toLowerCase() === 'true'))
+
+if (TESTNET && REGTEST) {
+  throw new Error('Cannot specify REGTEST and TESTNET at the same time')
+}
 
 let debug = require('debug')('index')
 let express = require('express')
@@ -13,7 +18,7 @@ let api = require('./lib/express')
 let app = express()
 
 // run the service
-debug(`Initializing blockchain connection${TESTNET ? " (for testnet)" : ""}`)
+debug(`Initializing blockchain connection${["", " (for testnet)", " (for regtest)"][(TESTNET?1:0)+(REGTEST?2:0)]}`)
 service((err, adapter) => {
   if (err) {
     return debug('Initialization failed:', err)
@@ -21,7 +26,7 @@ service((err, adapter) => {
 
   // start the API server
   debug('starting API server')
-  app.use(api(adapter, {testnet: TESTNET}))
+  app.use(api(adapter, {testnet: TESTNET, regtest: REGTEST}))
   app.listen(process.env.SERVER_PORT);
   debug("App listening on port "+process.env.SERVER_PORT);
 })
