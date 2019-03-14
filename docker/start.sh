cb713884e521491022096f8217a65e207a325159
John Villar <johnvillar@contimita.com>
2019-03-14 11:00:58 -0400

    Exposed the regtest port, added better leveldb searchRange functionality, fixed some scripts

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 cd /indexd
+
+if [ ! -f ./node_modules/bitcoinjs-lib/package.json ]; then
+  echo "No nodejs modules installed, installing"
+  npm install
+fi
+
+if [ ! -f ./.env ]; then
+  cp .env-example .env
+fi
+
 /usr/bin/forever ./index.js
